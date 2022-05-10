19b3cc7fd727961651a997929b02ae698a805e7a
BattlerHaru <kronk.sdance@gmail.com>
2022-05-10 13:36:38 -0500

    SOLID - Open and Close - se removio la libreria axios para obtener informacion de otra forma

--- src/solid/02-oc/c.ts
+++ src/solid/02-oc/c.ts
@@ -1,9 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 export class HttpClient {
+    // async get(url: string) {
+    //     const { data } = await axios.get(url);
+    //     return data;
+    // }
+
     async get(url: string) {
-        const { data } = await axios.get(url);
-        return data;
+        const resp = await fetch(url)
+        const data = await resp.json();
+
+        return { data }
     }
 
 }
